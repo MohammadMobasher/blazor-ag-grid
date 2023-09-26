aa4b3762082366d9a1bde9fc76d17394e9012f0b
MohammadMobasher <m.mobasher.z@gmail.com>
2023-09-26 07:49:58 +0330

    remove some useless imports from AgGrid class

--- src/BlazorAgGrid/AgGrid.razor.cs
+++ src/BlazorAgGrid/AgGrid.razor.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace BlazorAgGrid
 {
