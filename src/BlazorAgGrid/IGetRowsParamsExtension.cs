fc995b462a6d5f14c2dd1cdf237f253d7a35df81
MohammadMobasher <m.mobasher.z@gmail.com>
2023-09-30 16:21:15 +0330

    create an extension to convert string json to object

---
+++ src/BlazorAgGrid/IGetRowsParamsExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace BlazorAgGrid
+{
+    public static class IGetRowsParamsExtension
+    {
+        public static T? FileterModelToObject<T>(this IGetRowsParams input)
+        {
+            return JsonSerializer.Deserialize<T>(input.StringFilterModel);
+        }
+    }
+}
